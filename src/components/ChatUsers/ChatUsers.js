5166fb094a54e73e57fe52ce9254a47676bbb123
scal3 <cool.goga501@gmail.com>
2022-01-28 15:40:53 +0300

    feat/create ChatUsers layout

--- src/components/ChatUsers/ChatUsers.js
+++ src/components/ChatUsers/ChatUsers.js
@@ -5,9 +5,43 @@ import './ChatUsers.css'
 function ChatUsers() {
   return (
     <div className="chat-users">
-      <h2 className="chat-users__heading">Users(1):</h2>
+      <div className="chat-users__sticky-heading">
+        <h2 className="chat-users__heading">Users(1):</h2>
+      </div>
       <ul className="chat-users__list">
-
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
+        <li className="chat-users__list-item">Вовчик</li>
+        <li className="chat-users__list-item">Саня</li>
+        <li className="chat-users__list-item">Семён</li>
       </ul>
     </div>
   )
