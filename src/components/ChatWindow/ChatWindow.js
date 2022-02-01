1e970514c6ae7bad183373b07a5298c5f35c5c28
scal3 <cool.goga501@gmail.com>
2022-02-01 20:38:27 +0300

    fix/change input on textarea

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -1,3 +1,36 @@
+// import React from 'react';
+
+// import './ChatWindow.css'
+
+// function ChatWindow() {
+//   return (
+//     <div className="chat-window">
+//       <div className="chat-window__display">
+//         <div className="chat-window__message-container"> 
+//           <span className="chat-window__message-user">Anton</span>
+//           <div className="chat-window__message-background">
+//             <p className="chat-window__message-text">Hello, guys!</p>
+//           </div>
+//         </div>
+//         <div className="chat-window__message-container"> 
+//           <span className="chat-window__message-user">Anton</span>
+//           <div className="chat-window__message-background">
+//             <p className="chat-window__message-text">Hello, guys!jkdsbvdsbvjdfbvdfjvbdflvdflnvdflksvnldfnvdflknvdlfk;nvl;dfnvdfvnkdfnvdfkvn</p>
+//           </div>
+//         </div>
+//       </div>
+//       <form className="chat-window__form">
+//         <input className="chat-window__input" type="text" placeholder="Enter your message"/>
+//         <button className="chat-window__button" type="submit"></button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default ChatWindow;
+
+
+
 import React from 'react';
 
 import './ChatWindow.css'
@@ -20,7 +53,7 @@ function ChatWindow() {
         </div>
       </div>
       <form className="chat-window__form">
-        <input className="chat-window__input" type="text" placeholder="Enter your message"/>
+        <textarea className="chat-window__text-area" type="text" placeholder="Enter your message"/>
         <button className="chat-window__button" type="submit"></button>
       </form>
     </div>
