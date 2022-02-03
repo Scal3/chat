6c927f05150a72abe7dbeac676c44055c5ba98c6
scal3 <cool.goga501@gmail.com>
2022-02-03 21:03:47 +0300

    fix/initial data acquisition on front

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,65 @@
+// import { useReducer, useEffect } from 'react'
+// import axios from 'axios';
+
+// import './App.css';
+
+// import Login from '../Login/Login';
+// import Chat from '../Chat/Chat';
+// import socket from '../../utils/socket';
+// import reducer from '../../utils/reducer'
+
+
+// function App() {
+//   const [state, dispatch] = useReducer(reducer, {
+//     joined: false,
+//     room: null,
+//     userName: null,
+//     users: [],
+//     messages: []
+//   })
+
+
+//   const setUsers = (users) => {
+//     dispatch({
+//       type: 'SET_USERS',
+//       payload: users
+//     })
+//   }
+
+//   useEffect(() => {
+//     socket.on('ROOM:SET_USERS', setUsers)
+//   }, [])
+
+//   const onLogin = (userData) => {
+//     dispatch({
+//       type: 'JOINED',
+//       payload: userData
+//     })
+//     socket.emit('ROOM:JOIN', userData)
+//     axios.get(`/rooms/${userData.room}`)
+//   }
+
+//   return (
+//     <div className="app">
+//       {state.joined ? <Chat users={state.users} messages={state.messages}/> : <Login onLogin={onLogin}/>}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import { useReducer, useEffect } from 'react'
+import axios from 'axios';
+
 import './App.css';
+
 import Login from '../Login/Login';
 import Chat from '../Chat/Chat';
 import socket from '../../utils/socket';
-import { useReducer, useEffect } from 'react'
 import reducer from '../../utils/reducer'
 
 
@@ -24,16 +81,17 @@ function App() {
   }
 
   useEffect(() => {
-    socket.on('ROOM:JOINED', setUsers)
     socket.on('ROOM:SET_USERS', setUsers)
   }, [])
 
-  const onLogin = (userData) => {
+  const onLogin = async (userData) => {
     dispatch({
       type: 'JOINED',
       payload: userData
     })
     socket.emit('ROOM:JOIN', userData)
+    const { data } = await axios.get(`/rooms/${userData.room}`)
+    setUsers(data.roomData.users) // try/catch
   }
 
   return (
--- src/components/ChatUsers/ChatUsers.js
+++ src/components/ChatUsers/ChatUsers.js
@@ -3,9 +3,6 @@ import React from 'react';
 import './ChatUsers.css'
 
 function ChatUsers({ users }) {
-
-  console.log(users)
-
   return (
     <div className="chat-users">
       <h2 className="chat-users__heading">Online({users.length}):</h2>
