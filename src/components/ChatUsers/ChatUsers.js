e532e6585a537e7233f4fc54113651079c701e8e
scal3 <cool.goga501@gmail.com>
2022-03-27 13:15:06 +0300

    fix/add time to message

--- src/components/ChatUsers/ChatUsers.js
+++ src/components/ChatUsers/ChatUsers.js
@@ -11,7 +11,7 @@ const ChatUsers = () => {
 
   return (
     <div className="chat-users">
-      <h2 className="chat-users__heading">Комната: {room}</h2>
+      <h2 className="chat-users__heading">Room: {room}</h2>
       <h2 className="chat-users__heading">Online({users.length}):</h2>
       <ul className="chat-users__list">
         { users.map((user, i) => (
--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -8,18 +8,30 @@ import { getMessages, getUserName, getRoom } from '../../selectors/selectors'
 import { addMessage } from '../../actions/actions'
 
 const ChatWindow = () => {
+  const date = new Date()
+  const currentDate = `${date.getHours()}:${date.getMinutes()}`
   const dispatch = useDispatch()
   const messages = useSelector(getMessages)
   const userName = useSelector(getUserName)
   const room = useSelector(getRoom)
 
   const [chatMessage, setChatMessage] = React.useState('')
+  const [isDisabled, setIsDisabled] = React.useState(false)
   const messagesRef = useRef(null)
 
+  console.log(messages)
 
   useEffect(() => {
     messagesRef.current.scrollTo(0, 99999)
   }, [messages])
+
+  useEffect(() => {
+    if(!chatMessage.trim()) {
+      setIsDisabled(true)
+    } else {
+      setIsDisabled(false)
+    }
+  }, [chatMessage])
 
   const handleTextarea = (event) => setChatMessage(event.target.value)
 
@@ -28,9 +40,10 @@ const ChatWindow = () => {
     socket.emit('ROOM:NEW_MESSAGE', {
       room,
       userName, 
-      text: chatMessage
+      text: chatMessage.trim(),
+      currentDate
     })
-    dispatch(addMessage({userName, text: chatMessage}))
+    dispatch(addMessage({ userName, text: chatMessage, currentDate }))
     setChatMessage('')
   }
 
@@ -43,6 +56,7 @@ const ChatWindow = () => {
               <div className="chat-window__message-container chat-window__message-container_type_current-user" key={i}> 
               <div className="chat-window__message-background">
                 <p className="chat-window__message-text">{message.text}</p>
+                <p className="chat-window__message-date">{message.currentDate}</p>
               </div>
             </div>
             ) : (
@@ -50,6 +64,7 @@ const ChatWindow = () => {
               <span className="chat-window__message-user">{message.userName}</span>
               <div className="chat-window__message-background">
                 <p className="chat-window__message-text">{message.text}</p>
+                <p className="chat-window__message-date">{message.currentDate}</p>
               </div>
             </div>
             )
@@ -59,7 +74,12 @@ const ChatWindow = () => {
       </div>
       <form className="chat-window__form" onSubmit={handleSubmit}>
         <textarea className="chat-window__text-area" type="text" placeholder="Write your message" rows="1" value={chatMessage} onChange={handleTextarea}/>
-        <button className="chat-window__button" type="submit"></button>
+        { isDisabled 
+          ?
+          <button className="chat-window__button" type="submit" disabled></button>
+          :
+          <button className="chat-window__button" type="submit"></button>
+        }
       </form>
     </div>
   )
